b9595fbbcea8d680bd02dffd16a23e6e5fc2ad95
blurr <blurr@middle-earth.us>
2009-08-01 14:56:43 +0100

    Added Rancid Snakebite Tavern to the do_who() command.

--- pp/src/act.informative.cpp
+++ pp/src/act.informative.cpp
@@ -7987,6 +7987,10 @@ void do_who (CHAR_DATA * ch, char *argument, int cmd)
 		roomCount(rd);
 		if (rd->occupants > 0)
 			whoStream << "In the Copper Tankard Tavern, there " << (rd->occupants == 1 ? "is#2 " : "are#2 ") << rd->occupants << (rd->occupants == 1 ? " #0player. " : " #0players. ") << std::endl;
+		rd = vtor(21170);
+		roomCount(rd);
+		if (rd->occupants > 0)
+			whoStream << "In the Commons of the Rancid Snakebute Tavern, there " << (rd->occupants == 1 ? "is#2 " : "are#2 ") << rd->occupants << (rd->occupants == 1 ? " #0player. " : " #0players. ") << std::endl;
 		//Show battalion information to only those who can access it - Vader
 		if (is_clan_member(ch, "ithilien_battalion"))
 		{
