f927978e4106cbda0eb896923dc9e74f5b62c810
SerindeSoI <serinde@middle-earth.us>
2010-05-21 23:02:08 +1200

    Please use brackets like I do when editing my functions

--- pp/src/utility.cpp
+++ pp/src/utility.cpp
@@ -1390,8 +1390,9 @@ int first_free = 0;
 int mud_memory = 0;
 
 char* duplicateString(const char *source) {
-	if (source == NULL)
+	if (source == NULL) {
 		return NULL;
+	}
 	else if (strcmp(source, "") == STR_MATCH) {
 		static char *emptyString = "";
 		return emptyString;
