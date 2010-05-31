9d4f0751c9d8630d48cb2d3fa9ce4c0bb65dc1cc
grommit <myarta@gmail.com>
2010-05-31 00:47:24 -0400

    Fixed for stupid logic error in can_order

--- pp/src/commerce.cpp
+++ pp/src/commerce.cpp
@@ -1042,8 +1042,8 @@ can_order (OBJ_DATA * obj, CHAR_DATA * keeper)
 	if (temp)
 	  {
 	    int type = GET_ITEM_TYPE(temp);
-	    if (type == ITEM_ARMOR || type == ITEM_SHIELD || type == ITEM_WEAPON || type == ITEM_MISSILE)
-	      return (temp->instances >= 5); // if there are fewer than 5, this will be false and thus can't order
+	    if ((type == ITEM_ARMOR || type == ITEM_SHIELD || type == ITEM_WEAPON || type == ITEM_MISSILE) && temp->instances < 5)
+	      return false;
 	  }
 
 
